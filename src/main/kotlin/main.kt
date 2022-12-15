5818fad9811453f249cdc6763ef59da07c63e835
192.168.0.21 <antonov_vv@spbexp.ru>
2022-12-15 17:07:56 +0300

    Учёт условий коммиси по типу карт

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,83 @@
-fun main(){
-    val amount:Int = 8000;
-    var commission:Int = (amount/100*0.75).toInt();
-    if (commission<35)
-    {
-        commission = 35;
-    }
-    println("Коммисия за перевод составляет: $commission");
+fun main() {
+    var amount: Int = 8000;
+    var typeCard: String = "Visa"
+    var allSum: Int = 4000;
+
+    println(
+        "Коммисия за перевод $amount по карте $typeCard составляет: ${
+            getCommission(
+                typeCard,
+                sum = amount
+            )
+        }"
+    );
+    amount = 59900;
+    println(
+        "Коммисия за перевод $amount по карте $typeCard составляет: ${
+            getCommission(
+                typeCard,
+                sum = amount
+            )
+        }"
+    );
+
+
+    typeCard = "VK Pay";
+    println(
+        "Коммисия за перевод $amount по карте $typeCard составляет: ${
+            getCommission(
+                typeCard,
+                sum = amount
+            )
+        }"
+    );
+
+    typeCard = "Mastercard";
+    println(
+        "Коммисия за перевод $amount по карте $typeCard составляет: ${
+            getCommission(
+                typeCard,
+                allSum,
+                sum = amount
+            )
+        }"
+    );
+
+    allSum = 200000;
+    println(
+        "Коммисия за перевод $amount по карте $typeCard составляет: ${
+            getCommission(
+                typeCard,
+                allSum,
+                sum = amount
+            )
+        }"
+    );
 }
+
+fun getCommission(type: String, allSum: Int = 0, sum: Int): Int {
+    return when (type) {
+        "VK Pay" -> {
+            0;
+        }
+        "Visa", "Мир" -> {
+            var commission: Int = (sum * 0.0075).toInt();
+            return if (commission < 35) {
+                35;
+            } else {
+                commission;
+            }
+        }
+        "Mastercard", "Maestro" -> {
+            return if (allSum in 30..75000) {
+                0;
+            } else {
+                (sum * 0.006).toInt() + 20;
+            }
+        }
+        else -> 0;
+    }
+
+}
+
+
